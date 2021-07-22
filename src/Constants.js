ff9fbdf65084fa2e1359f564537e82f26f149c33
PraveenRoshane <praveenroshane123@gmail.com>
2021-07-22 22:23:09 +0530

    5th commit

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,5 @@
 export const API_URL = 'http://localhost:8080'
-export const JPA_API_URL = 'http://localhost:8080'
+//export const JPA_API_URL = 'http://localhost:8080'
+export const JPA_API_URL = 'https://freshland-dairy-demo-online-shop.azurewebsites.net/'
+
+// ServicePlan7326f3bc-4b8a-417c
