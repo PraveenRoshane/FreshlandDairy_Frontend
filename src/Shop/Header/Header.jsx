5e7cf5315bed8951c6e4513649ccbf5373d42e04
PraveenRoshane <praveenroshane123@gmail.com>
2021-07-23 00:18:03 +0530

    6th commite

--- src/Shop/Header/Header.jsx
+++ src/Shop/Header/Header.jsx
@@ -8,20 +8,20 @@ class Header extends Component {
     render() {
         const isUserLoggedin = Authentication.isUserLoggedin();
         return (
-            <div class="header2 bg-success-gradiant">
-                <div class="">
-                    <nav class="navbar navbar-expand-lg h2-nav"> <a class="navbar-brand" href="#">Freshland Dairy</a>
-                        <div class="collapse navbar-collapse hover-dropdown" id="header2">
-                            <ul class="navbar-nav">                                
-                                <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
-                                {isUserLoggedin && <li class="nav-item"><Link class="nav-link" to="/ProductList">Product List</Link></li>}
-                                <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+            <div className="header2 bg-success-gradiant">
+                <div className="">
+                    <nav className="navbar navbar-expand-lg h2-nav">Freshland Dairy
+                        <div className="collapse navbar-collapse hover-dropdown" id="header2">
+                            <ul className="navbar-nav">                                
+                                <li className="nav-item active"><Link className="nav-link">Home</Link></li>
+                                {isUserLoggedin && <li className="nav-item"><Link className="nav-link" to="/ProductList">Product List</Link></li>}
+                                <li className="nav-item"><Link className="nav-link">Features</Link></li>
+                                <li className="nav-item"><Link className="nav-link">About Us</Link></li>
+                                <li className="nav-item"><Link className="nav-link">Contact</Link></li>
                             </ul>
-                            <ul class="navbar-nav ml-auto">
-                                {!isUserLoggedin && <li class="nav-item active"><Link class="nav-link" to="/login">Login</Link></li>}
-                                {isUserLoggedin && <li class="nav-item"><Link class="btn rounded-pill btn-dark py-2 px-4" to="/logout" onClick={Authentication.logout}>Logout</Link></li>}
+                            <ul className="navbar-nav ml-auto">
+                                {!isUserLoggedin && <li className="nav-item active"><Link className="nav-link" to="/login">Login</Link></li>}
+                                {isUserLoggedin && <li className="nav-item"><Link className="btn rounded-pill btn-dark py-2 px-4" to="/logout" onClick={Authentication.logout}>Logout</Link></li>}
                             </ul>
                         </div>
                     </nav>
